f7b41200201367b6edba171ce19dc2ddd7168ed3
032nnxkitty <aguznenkovv@yandex.ru>
2023-05-05 19:56:58 +0200

    Initial commit

---
+++ CompassApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompassApp
+//
+//  Created by Arseniy Zolotarev on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
