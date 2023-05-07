42c3ba03ae3264f780687d72926822e858e6d91b
032nnxkitty <aguznenkovv@yandex.ru>
2023-05-07 16:34:07 +0200

    Add unit tests to coordinates converting

---
+++ CompassAppTests/CompassAppTests.swift
@@ -0,0 +1,68 @@
+//
+//  CompassAppTests.swift
+//  CompassAppTests
+//
+//  Created by Arseniy Zolotarev on 07.05.2023.
+//
+
+import XCTest
+@testable import CompassApp
+
+final class CompassAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+    
+    func testPragueCoordinatesFormatting() {
+        // Given
+        let latitude: Double = 50.080050
+        let longitude: Double = 14.429350
+        let expectedLatitude = "50° 04' 48'' N"
+        let expectedLongitude = "14° 25' 45'' E"
+        
+        // When
+        let formattedLatitude = latitude.formatLatitude()
+        let formattedLongitude = longitude.formatLongitude()
+        
+        // Then
+        XCTAssertEqual(formattedLatitude, expectedLatitude)
+        XCTAssertEqual(formattedLongitude, expectedLongitude)
+    }
+    
+    func testSydneyCoordinatesFormatting() {
+        // Given
+        let latitude: Double = -33.852241
+        let longitude: Double = 151.196326
+        let expectedLatitude = "33° 51' 08'' S"
+        let expectedLongitude = "151° 11' 46'' E"
+        
+        // When
+        let formattedLatitude = latitude.formatLatitude()
+        let formattedLongitude = longitude.formatLongitude()
+        
+        // Then
+        XCTAssertEqual(formattedLatitude, expectedLatitude)
+        XCTAssertEqual(formattedLongitude, expectedLongitude)
+    }
+    
+    func testDenverCoordinatesFormatting() {
+        // Given
+        let latitude: Double = 39.748246
+        let longitude: Double = -104.949006
+        let expectedLatitude = "39° 44' 53'' N"
+        let expectedLongitude = "104° 56' 56'' W"
+        
+        // When
+        let formattedLatitude = latitude.formatLatitude()
+        let formattedLongitude = longitude.formatLongitude()
+        
+        // Then
+        XCTAssertEqual(formattedLatitude, expectedLatitude)
+        XCTAssertEqual(formattedLongitude, expectedLongitude)
+    }
+}
